ca02af57e9b661497e89d0e0b6f4c65b0bc9c815
Sebastian Slomski <me@sebslomski.com>
2013-02-28 09:52:04 +0100

    init

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='German-Bank-Holidays',
+    version='0.1',
+    url='http://github.com/sebslomski/german-bank-holidays',
+    license='MIT',
+    author='Sebastian Slomski',
+    author_email='me@sebslomski.com',
+    description='German national bank holidays',
+    packages=['german_bank_holidays'],
+    zip_safe=False,
+    platforms='any'
+)
